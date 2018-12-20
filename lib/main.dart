da7f73e8bc8403bc4c4498dc92fd5327332523b7
Aritra Das <adas@vsoftconsulting.com>
2018-12-20 16:22:58 +0530

    added screenshots and made fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:open_ngo/view/login/login_page.dart';
 
 void main() => runApp(MyApp());
@@ -7,6 +8,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       title: 'Open NGO',
       theme: ThemeData(
